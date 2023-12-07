5baca59db8434fef9f796f5153cd99cec66e36b9
Alecoronado <153213444+Alecoronado@users.noreply.github.com>
2023-12-07 22:39:31 +0000

    www

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,84 +1,197 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from typing import Any
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
+import re
+from datetime import datetime
+from dateutil import parser
+
+# Configuración inicial de la página
+st.set_page_config(page_title="Análisis de Eficiencia Operativa", page_icon="📊")
+
+# URL del Google Sheet en formato CSV
+# Asegúrate de reemplazar 'SheetName' con el nombre real de tu hoja
+sheet_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=918102047&single=true&output=csv"
+sheet_operaciones_url_csv="https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=1958213072&single=true&output=csv"
+sheet_desembolsos_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=1839704968&single=true&output=csv"
+
+# Función para cargar los datos desde la URL
+def load_data_from_url(url):
+    try:
+        return pd.read_csv(url, header=0)
+    except Exception as e:
+        st.error("Error al cargar los datos: " + str(e))
+        return None
+
+# Función para convertir las fechas del formato español al formato estándar
+def convert_spanish_date(date_str):
+    months = {
+        'ENE': 'Jan', 'FEB': 'Feb', 'MAR': 'Mar', 'ABR': 'Apr', 'MAY': 'May', 'JUN': 'Jun',
+        'JUL': 'Jul', 'AGO': 'Aug', 'SEP': 'Sep', 'OCT': 'Oct', 'NOV': 'Nov', 'DIC': 'Dec'
+    }
+    match = re.match(r"(\d{2}) (\w{3}) (\d{2})", date_str)
+    if match:
+        day, spanish_month, year = match.groups()
+        english_month = months.get(spanish_month.upper())
+        if english_month:
+            return datetime.strptime(f"{day} {english_month} 20{year}", "%d %b %Y").strftime("%d/%m/%Y")
+    return date_str
+
+def calculate_kpi(end_date, start_date):
+    try:
+        # Convertir a datetime si son cadenas
+        if isinstance(start_date, str):
+            start_date = pd.to_datetime(start_date, errors='coerce')
+        if isinstance(end_date, str):
+            end_date = pd.to_datetime(end_date, errors='coerce')
+
+        if pd.isnull(start_date) or pd.isnull(end_date):
+            return None
+
+        kpi_value = round(((end_date - start_date).days / 30), 2)
+        if kpi_value < 0:  # Comprobar si el KPI es negativo
+            return None  # Devolver None si el KPI es negativo
+        return kpi_value
+    except Exception as e:
+        # Manejar cualquier otro error inesperado
+        st.error(f"Error al calcular KPI: {e}")
+        return None
+
+def calculate_productivity(kpi):
+    if kpi is None:
+        return "Datos insuficientes"
+    if kpi < 6:
+        return "Eficiente"
+    elif kpi < 8:
+        return "Aceptable"
+    elif kpi < 12:
+        return "Con Demora"
+    else:
+        return "Alta Demora"
+
+def get_year_for_operation(date_str):
+    if pd.notnull(date_str):
+        try:
+            # Parsear la fecha y devolver el año
+            return parser.parse(date_str).year
+        except (ValueError, TypeError):
+            # Devuelve None si hay un problema al parsear la fecha
+            return None
+    else:
+        return None
+
+def get_first_word(station):
+    return station.split()[0] if station else None
+
+# Función para preprocesar y estandarizar las fechas
+def preprocess_date(date_str):
+    try:
+        # Intenta parsear la fecha con dateutil.parser que es flexible con los formatos
+        dt = parser.parse(date_str)
+        # Formatea la fecha al formato día/mes/año
+        return dt.strftime('%d/%m/%Y')
+    except (ValueError, TypeError):
+        # Devuelve None si hay un problema al parsear la fecha
+        return None
+
+def run(filtered_df):
+    # Asegúrate de que 'filtered_df' contiene tus datos
+    if filtered_df is not None:
+        data = filtered_df.copy()
+
+        # Convertir las columnas de fecha a datetime si aún no lo están
+        date_columns = ['CARTA CONSULTA', 'APROBACIÓN', 'FechaVigencia', 'FechaEfectiva']
+        for col in date_columns:
+            data[col] = pd.to_datetime(data[col], errors='coerce')
+
+        # Mapeo de estaciones a sus respectivas columnas de fecha
+        operations = {
+            'Vigencia - Aprobacion': ('APROBACIÓN', 'FechaVigencia'),
+            'Aprobacion - Carta Consulta': ('CARTA CONSULTA', 'APROBACIÓN'),
+            'Elegibilidad - Vigencia': ('FechaVigencia', 'FechaElegibilidad'),
+            'PrimerDesembolso - Elegibilidad': ('FechaEfectiva', 'FechaElegibilidad')
+        }
+
+        # Lista para almacenar los resultados
+        results = []
+
+        # Procesar las filas del DataFrame
+        for index, row in data.iterrows():
+            for operation, (start_col, end_col) in operations.items():
+                kpi = calculate_kpi(row[end_col], row[start_col])
+                productividad = calculate_productivity(kpi)
+                year = get_year_for_operation(row[end_col])
+
+                # Usar strftime solo si la fecha es un objeto datetime
+                indicador_principal = row[end_col].strftime('%d/%m/%Y') if pd.notnull(row[end_col]) and isinstance(row[end_col], datetime) else None
+                indicador_secundario = row[start_col].strftime('%d/%m/%Y') if pd.notnull(row[start_col]) and isinstance(row[start_col], datetime) else None
+
+                results.append({
+                    'ESTACIONES': get_first_word(operation),
+                    'ANO': year,
+                    'PAIS': row['Pais'],
+                    'CODIGO': row['NoOperacion'],
+                    'APODO': row['Alias'],
+                    'Indicador_Principal': indicador_principal,
+                    'Indicador_Secundario': indicador_secundario,
+                    'TIPO_DE_KPI': operation,
+                    'KPI': kpi,
+                    'Productividad': productividad
+                })
+
+        # Convertir la lista de resultados en un DataFrame
+        results_df = pd.DataFrame(results)
+
+        # Limpiar los valores negativos o None de KPI y establecerlos en None
+        results_df['KPI'] = results_df['KPI'].apply(lambda x: x if x is not None and x >= 0 else None)
+
+        # Filtrar para mantener solo las filas donde el KPI es mayor a 0
+        results_df = results_df[results_df['KPI'] > 0]
+
+        # Mostrar el DataFrame de resultados
+        st.write(results_df)
 
 
-def animation_demo() -> None:
+# Aplicación Streamlit
+def main():
+    st.title("Mi Aplicación con Datos de Google Sheets")
 
-    # Interactive Streamlit elements, like these sliders, return their value.
-    # This gives you an extremely simple interaction model.
-    iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-    separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
+    # Carga los datos
+    data = load_data_from_url(sheet_url_csv)
+    data_operaciones = load_data_from_url(sheet_operaciones_url_csv)
+    data_desembolsos = load_data_from_url(sheet_desembolsos_url_csv)
 
-    # Non-interactive elements return a placeholder to their location
-    # in the app. Here we're storing progress_bar to update it later.
-    progress_bar = st.sidebar.progress(0)
+    if data is not None and data_operaciones is not None and data_desembolsos is not None:
+        # Procesamiento de datos
+        date_columns = ['ABSTRACTO', 'CARTA CONSULTA', 'PERFIL', 'PROPUESTA OPERATIVA', 'ACTA NEGOCIACION', 'APROBACIÓN']
+        for col in date_columns:
+            data[col] = data[col].apply(lambda x: convert_spanish_date(x) if isinstance(x, str) else x)
+        data['NO. OPERACION'] = data['NO. OPERACION'].str.replace('-', '', regex=False)
+        data['NÚMERO'] = data['NÚMERO'].str.replace('-', '', regex=False)
+        data.rename(columns={'NÚMERO': 'NoProyecto'}, inplace=True)
+        data.rename(columns={'NO.OPERACION': 'NoOperacion'}, inplace=True)
 
-    # These two elements will be filled in later, so we create a placeholder
-    # for them using st.empty()
-    frame_text = st.sidebar.empty()
-    image = st.empty()
+        # Unión de los datos
+        data_merged = pd.merge(data, data_operaciones, on='NoProyecto', how='inner')
+        data_merged_total = pd.merge(data_merged, data_desembolsos, on='NoOperacion', how='inner')
 
-    m, n, s = 960, 640, 400
-    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
+        # Filtrar el DataFrame para conservar solo las columnas seleccionadas
+        selected_columns = [
+            'NoProyecto', 'NoOperacion', 'Pais', 'Alias', 'SEC', 'ARE', 
+            'CARTA CONSULTA', 'APROBACIÓN', 'PERFIL', 'PROPUESTA OPERATIVA', 
+            'FechaVigencia', 'FechaElegibilidad', 'FechaEfectiva', 'Estado_x'
+        ]
+        filtered_df = data_merged_total[selected_columns]
 
-    for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-        # Here were setting value for these two elements.
-        progress_bar.progress(frame_num)
-        frame_text.text("Frame %i/100" % (frame_num + 1))
+        # Preprocesar las fechas en el DataFrame filtrado
+        date_columns = ['FechaVigencia', 'FechaElegibilidad', 'FechaEfectiva']
+        for date_col in date_columns:
+            filtered_df[date_col] = filtered_df[date_col].apply(preprocess_date)
 
-        # Performing some fractal wizardry.
-        c = separation * np.exp(1j * a)
-        Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
-        C = np.full((n, m), c)
-        M: Any = np.full((n, m), True, dtype=bool)
-        N = np.zeros((n, m))
+        # Mostrar el DataFrame con las fechas preprocesadas
+        st.write(filtered_df)
 
-        for i in range(iterations):
-            Z[M] = Z[M] * Z[M] + C[M]
-            M[np.abs(Z) > 2] = False
-            N[M] = i
+        # Llama a la función run para realizar el análisis adicional
+        run(filtered_df)
 
-        # Update the image placeholder by calling the image() function on it.
-        image.image(1.0 - (N / N.max()), use_column_width=True)
+if __name__ == "__main__":
+    main()
 
-    # We clear elements by calling empty on them.
-    progress_bar.empty()
-    frame_text.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
-
-
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
-st.sidebar.header("Animation Demo")
-st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
-)
-
-animation_demo()
-
-show_code(animation_demo)
--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -1,56 +1,148 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-import time
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
+import re
+from datetime import datetime
+
+# Configuración inicial de la página
+st.set_page_config(page_title="Análisis de Eficiencia Operativa", page_icon="📊")
+
+# Asegúrate de reemplazar 'SheetName' con el nombre real de tu hoja
+sheet_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=918102047&single=true&output=csv"
+sheet_operaciones_url_csv="https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=1958213072&single=true&output=csv"
+sheet_desembolsos_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=1839704968&single=true&output=csv"
 
 
-def plotting_demo():
-    progress_bar = st.sidebar.progress(0)
-    status_text = st.sidebar.empty()
-    last_rows = np.random.randn(1, 1)
-    chart = st.line_chart(last_rows)
+# Función para cargar los datos desde la URL
+def load_data_from_url(url):
+    try:
+        return pd.read_csv(url, header=0)
+    except Exception as e:
+        st.error("Error al cargar los datos: " + str(e))
+        return None
 
-    for i in range(1, 101):
-        new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
-        status_text.text("%i%% Complete" % i)
-        chart.add_rows(new_rows)
-        progress_bar.progress(i)
-        last_rows = new_rows
-        time.sleep(0.05)
+# Función para convertir las fechas del formato español al formato estándar
+def convert_spanish_date(date_str):
+    months = {
+        'ENE': 'Jan', 'FEB': 'Feb', 'MAR': 'Mar', 'ABR': 'Apr', 'MAY': 'May', 'JUN': 'Jun',
+        'JUL': 'Jul', 'AGO': 'Aug', 'SEP': 'Sep', 'OCT': 'Oct', 'NOV': 'Nov', 'DIC': 'Dec'
+    }
+    match = re.match(r"(\d{2}) (\w{3}) (\d{2})", date_str)
+    if match:
+        day, spanish_month, year = match.groups()
+        english_month = months.get(spanish_month.upper())
+        if english_month:
+            return datetime.strptime(f"{day} {english_month} 20{year}", "%d %b %Y").strftime("%d/%m/%Y")
+    return date_str
 
-    progress_bar.empty()
+# Funciones para el cálculo de KPI y Productividad
+def calculate_kpi(end_date, start_date):
+    if pd.isnull(start_date) or pd.isnull(end_date):
+        return None
+    return round(((end_date - start_date).days / 30), 2)
 
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
+def calculate_productivity(kpi):
+    if kpi is None:
+        return "Datos insuficientes"
+    if kpi < 6:
+        return "Eficiente"
+    elif kpi < 8:
+        return "Aceptable"
+    elif kpi < 12:
+        return "Con Demora"
+    else:
+        return "Alta Demora"
+
+def get_year_for_operation(date):
+    return date.year if pd.notnull(date) else None
+
+def get_first_word(station):
+    return station.split()[0] if station else None
+
+# Aplicación Streamlit
+def main():
+    st.title("Mi Aplicación con Datos de Google Sheets")
+
+    # Carga los datos
+    data = load_data_from_url(sheet_url_csv)
+    data_operaciones = load_data_from_url(sheet_operaciones_url_csv)
+    data_desembolsos = load_data_from_url(sheet_desembolsos_url_csv)
+
+    if data is not None and data_operaciones is not None and data_desembolsos is not None:
+        # Procesamiento de datos
+        date_columns = ['ABSTRACTO', 'CARTA CONSULTA', 'PERFIL', 'PROPUESTA OPERATIVA', 'ACTA NEGOCIACION', 'APROBACIÓN']
+        for col in date_columns:
+            data[col] = data[col].apply(lambda x: convert_spanish_date(x) if isinstance(x, str) else x)
+        data['NO. OPERACION'] = data['NO. OPERACION'].str.replace('-', '', regex=False)
+        data['NÚMERO'] = data['NÚMERO'].str.replace('-', '', regex=False)
+        data.rename(columns={'NÚMERO': 'NoProyecto'}, inplace=True)
+        data.rename(columns={'NO.OPERACION': 'NoOperacion'}, inplace=True)
+
+        # Unión de los datos
+        data_merged = pd.merge(data, data_operaciones, on='NoProyecto', how='inner')
+        data_merged_total = pd.merge(data_merged, data_desembolsos, on='NoOperacion', how='inner')
+
+        # Filtrar el DataFrame para conservar solo las columnas seleccionadas
+        selected_columns = [
+            'NoProyecto', 'NoOperacion', 'Pais', 'Alias', 'SEC', 'ARE', 
+            'CARTA CONSULTA', 'APROBACIÓN', 'PERFIL', 'PROPUESTA OPERATIVA', 
+            'FechaVigencia', 'FechaEfectiva', 'Estado_x'
+        ]
+        filtered_df = data_merged_total[selected_columns]
+
+        # Mostrar el nuevo DataFrame filtrado
+        st.write(filtered_df)
+
+        # Llama a la función run para realizar el análisis
+        run(filtered_df)
+
+# Función principal de análisis en la app de Streamlit
+def run(filtered_df):
+    # Asegúrate de que 'filtered_df' contiene tus datos
+    if filtered_df is not None:
+        data = filtered_df.copy()
+
+        # Convertir las columnas de fecha a datetime y extraer el año
+        date_columns = ['CARTA CONSULTA', 'APROBACIÓN', 'FechaVigencia', 'FechaEfectiva']
+        for col in date_columns:
+            data[col] = pd.to_datetime(data[col], errors='coerce')
+
+        # Mapeo de estaciones a sus respectivas columnas de fecha
+        operations = {
+            'Elegibilidad - Vigencia': ('FechaVigencia', 'FechaElegibilidad'),  # Ajusta 'FechaElegibilidad' si es necesario
+            'PrimerDesembolso - Elegibilidad': ('FechaElegibilidad', 'FechaEfectiva'),  # Ajusta 'FechaElegibilidad' si es necesario
+            'Vigencia - Aprobacion': ('APROBACIÓN', 'FechaVigencia'),
+            'Aprobacion - Carta Consulta': ('CARTA CONSULTA', 'APROBACIÓN')
+        }
+
+        # Lista para almacenar los resultados
+        results = []
+
+        # Procesar las filas del DataFrame
+        for index, row in data.iterrows():
+            for operation, (start_col, end_col) in operations.items():
+                kpi = calculate_kpi(row[end_col], row[start_col])
+                productividad = calculate_productivity(kpi)
+                year = get_year_for_operation(row[end_col])
+                results.append({
+                    'ESTACIONES': get_first_word(operation),
+                    'ANO': year,
+                    'PAIS': row['Pais'],
+                    'CODIGO': row['NoOperacion'],
+                    'APODO': row['Alias'],
+                    'Indicador_Principal': row[end_col].strftime('%d/%m/%Y') if pd.notnull(row[end_col]) else None,
+                    'Indicador_Secundario': row[start_col].strftime('%d/%m/%Y') if pd.notnull(row[start_col]) else None,
+                    'TIPO_DE_KPI': operation,
+                    'KPI': kpi,
+                    'Productividad': productividad
+                })
+
+        # Convertir la lista de resultados en un DataFrame
+        results_df = pd.DataFrame(results)
+
+        # Mostrar el DataFrame de resultados
+        st.write(results_df)
+
+if __name__ == "__main__":
+    main()
 
 
-st.set_page_config(page_title="Plotting Demo", page_icon="📈")
-st.markdown("# Plotting Demo")
-st.sidebar.header("Plotting Demo")
-st.write(
-    """This demo illustrates a combination of plotting and animation with
-Streamlit. We're generating a bunch of random numbers in a loop for around
-5 seconds. Enjoy!"""
-)
-
-plotting_demo()
-
-show_code(plotting_demo)

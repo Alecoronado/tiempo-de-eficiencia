991823e9fbd21663ac2ddaa9facc1f417a781e82
Alecoronado <153213444+Alecoronado@users.noreply.github.com>
2023-12-08 18:02:20 +0000

    aa

--- pages/1_Plotting_Demo.py
+++ pages/1_Plotting_Demo.py
@@ -7,7 +7,7 @@ from datetime import datetime
 st.set_page_config(page_title="Análisis de Eficiencia Operativa", page_icon="📊")
 
 # Asegúrate de reemplazar 'SheetName' con el nombre real de tu hoja
-sheet_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=918102047&single=true&output=csv"
+sheet_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?output=csv"
 sheet_operaciones_url_csv="https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=1958213072&single=true&output=csv"
 sheet_desembolsos_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=1839704968&single=true&output=csv"
 
@@ -34,29 +34,45 @@ def convert_spanish_date(date_str):
             return datetime.strptime(f"{day} {english_month} 20{year}", "%d %b %Y").strftime("%d/%m/%Y")
     return date_str
 
-# Funciones para el cálculo de KPI y Productividad
-def calculate_kpi(end_date, start_date):
-    if pd.isnull(start_date) or pd.isnull(end_date):
+# Función para manejar diferentes formatos de fechas y valores nulos
+def convert_dates(date_str):
+    if pd.isnull(date_str):
         return None
-    return round(((end_date - start_date).days / 30), 2)
 
-def calculate_productivity(kpi):
-    if kpi is None:
-        return "Datos insuficientes"
-    if kpi < 6:
-        return "Eficiente"
-    elif kpi < 8:
-        return "Aceptable"
-    elif kpi < 12:
-        return "Con Demora"
-    else:
-        return "Alta Demora"
+    if not isinstance(date_str, str):
+        return date_str
 
-def get_year_for_operation(date):
-    return date.year if pd.notnull(date) else None
+    months = {
+        'ene': '01', 'feb': '02', 'mar': '03', 'abr': '04', 'may': '05', 'jun': '06',
+        'jul': '07', 'ago': '08', 'sep': '09', 'oct': '10', 'nov': '11', 'dic': '12'
+    }
 
-def get_first_word(station):
-    return station.split()[0] if station else None
+    try:
+        # Formato '15-ago-14' o '1-dic-17'
+        day, month, year = date_str.split('-')
+        if len(year) == 2: year = f"20{year}"
+        month = months.get(month[:3].lower(), '00')
+        return f"{day.zfill(2)}/{month}/{year}"
+    except ValueError:
+        pass
+
+    try:
+        # Formato 'martes, 17 de noviembre de 2015'
+        parts = date_str.split(' ')
+        day = parts[1]
+        month = parts[3].lower()[:3]
+        year = parts[5]
+        return f"{day.zfill(2)}/{months[month]}/{year}"
+    except (ValueError, IndexError):
+        pass
+
+    try:
+        # Formato '13-abr-20'
+        return datetime.strptime(date_str, '%d-%b-%y').strftime('%d/%m/%Y')
+    except ValueError:
+        pass
+
+    return date_str
 
 # Aplicación Streamlit
 def main():
@@ -78,69 +94,23 @@ def main():
         data.rename(columns={'NO.OPERACION': 'NoOperacion'}, inplace=True)
 
         # Unión de los datos
-        data_merged = pd.merge(data, data_operaciones, on='NoProyecto', how='inner')
-        data_merged_total = pd.merge(data_merged, data_desembolsos, on='NoOperacion', how='inner')
+        data_merged = pd.merge(data, data_operaciones, on='NoProyecto', how='left')
+        data_merged_total = pd.merge(data_merged, data_desembolsos, on='NoOperacion', how='left')
 
         # Filtrar el DataFrame para conservar solo las columnas seleccionadas
         selected_columns = [
             'NoProyecto', 'NoOperacion', 'Pais', 'Alias', 'SEC', 'ARE', 
-            'CARTA CONSULTA', 'APROBACIÓN', 'PERFIL', 'PROPUESTA OPERATIVA','FechaElegibilidad',
+            'CARTA CONSULTA', 'APROBACIÓN', 'PERFIL', 'PROPUESTA OPERATIVA', 'FechaElegibilidad',
             'FechaVigencia', 'FechaEfectiva', 'Estado_x'
         ]
         filtered_df = data_merged_total[selected_columns]
 
+        # Convertir las fechas en las columnas seleccionadas usando la nueva función
+        for col in ['FechaElegibilidad', 'FechaVigencia', 'FechaEfectiva']:
+            filtered_df[col] = filtered_df[col].apply(convert_dates)
+        
         # Mostrar el nuevo DataFrame filtrado
         st.write(filtered_df)
-
-        # Llama a la función run para realizar el análisis
-        run(filtered_df)
-
-# Función principal de análisis en la app de Streamlit
-def run(filtered_df):
-    # Asegúrate de que 'filtered_df' contiene tus datos
-    if filtered_df is not None:
-        data = filtered_df.copy()
-
-        # Convertir las columnas de fecha a datetime y extraer el año
-        date_columns = ['CARTA CONSULTA', 'APROBACIÓN','FechaElegibilidad', 'FechaVigencia', 'FechaEfectiva']
-        for col in date_columns:
-            data[col] = pd.to_datetime(data[col], errors='coerce')
-
-        # Mapeo de estaciones a sus respectivas columnas de fecha
-        operations = {
-            'Elegibilidad - Vigencia': ('FechaVigencia', 'FechaElegibilidad'),  # Ajusta 'FechaElegibilidad' si es necesario
-            'PrimerDesembolso - Elegibilidad': ('FechaElegibilidad', 'FechaEfectiva'),  # Ajusta 'FechaElegibilidad' si es necesario
-            'Vigencia - Aprobacion': ('APROBACIÓN', 'FechaVigencia'),
-            'Aprobacion - Carta Consulta': ('CARTA CONSULTA', 'APROBACIÓN')
-        }
-
-        # Lista para almacenar los resultados
-        results = []
-
-        # Procesar las filas del DataFrame
-        for index, row in data.iterrows():
-            for operation, (start_col, end_col) in operations.items():
-                kpi = calculate_kpi(row[end_col], row[start_col])
-                productividad = calculate_productivity(kpi)
-                year = get_year_for_operation(row[end_col])
-                results.append({
-                    'ESTACIONES': get_first_word(operation),
-                    'ANO': year,
-                    'PAIS': row['Pais'],
-                    'CODIGO': row['NoOperacion'],
-                    'APODO': row['Alias'],
-                    'Indicador_Principal': row[end_col].strftime('%d/%m/%Y') if pd.notnull(row[end_col]) else None,
-                    'Indicador_Secundario': row[start_col].strftime('%d/%m/%Y') if pd.notnull(row[start_col]) else None,
-                    'TIPO_DE_KPI': operation,
-                    'KPI': kpi,
-                    'Productividad': productividad
-                })
-
-        # Convertir la lista de resultados en un DataFrame
-        results_df = pd.DataFrame(results)
-
-        # Mostrar el DataFrame de resultados
-        st.write(results_df)
 
 if __name__ == "__main__":
     main()
--- pages/2_Mapping_Demo.py
+++ pages/2_Mapping_Demo.py
@@ -1,117 +1,87 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from urllib.error import URLError
-
-import pandas as pd
-import pydeck as pdk
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import pandas as pd
+import re
+from datetime import datetime as dt
 
+# Configuración inicial de la página
+st.set_page_config(page_title="Análisis de Eficiencia Operativa", page_icon="📊")
 
-def mapping_demo():
-    @st.cache_data
-    def from_data_file(filename):
-        url = (
-            "https://raw.githubusercontent.com/streamlit/"
-            "example-data/master/hello/v1/%s" % filename
-        )
-        return pd.read_json(url)
+# URLs de las hojas de Google Sheets
+sheet_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?output=csv"
+sheet_operaciones_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=1958213072&single=true&output=csv"
+sheet_desembolsos_url_csv = "https://docs.google.com/spreadsheets/d/e/2PACX-1vTG0WVV5FQNxYyOz0UM0YEkT9u8vGnzrwfUt7pVmJUHKGjDyKas_scI6XhY_ce_sTxRPtwVZw1Ggfyi/pub?gid=1839704968&single=true&output=csv"
 
+# Función para cargar los datos desde la URL
+def load_data_from_url(url):
     try:
-        ALL_LAYERS = {
-            "Bike Rentals": pdk.Layer(
-                "HexagonLayer",
-                data=from_data_file("bike_rental_stats.json"),
-                get_position=["lon", "lat"],
-                radius=200,
-                elevation_scale=4,
-                elevation_range=[0, 1000],
-                extruded=True,
-            ),
-            "Bart Stop Exits": pdk.Layer(
-                "ScatterplotLayer",
-                data=from_data_file("bart_stop_stats.json"),
-                get_position=["lon", "lat"],
-                get_color=[200, 30, 0, 160],
-                get_radius="[exits]",
-                radius_scale=0.05,
-            ),
-            "Bart Stop Names": pdk.Layer(
-                "TextLayer",
-                data=from_data_file("bart_stop_stats.json"),
-                get_position=["lon", "lat"],
-                get_text="name",
-                get_color=[0, 0, 0, 200],
-                get_size=10,
-                get_alignment_baseline="'bottom'",
-            ),
-            "Outbound Flow": pdk.Layer(
-                "ArcLayer",
-                data=from_data_file("bart_path_stats.json"),
-                get_source_position=["lon", "lat"],
-                get_target_position=["lon2", "lat2"],
-                get_source_color=[200, 30, 0, 160],
-                get_target_color=[200, 30, 0, 160],
-                auto_highlight=True,
-                width_scale=0.0001,
-                get_width="outbound",
-                width_min_pixels=3,
-                width_max_pixels=30,
-            ),
-        }
-        st.sidebar.markdown("### Map Layers")
-        selected_layers = [
-            layer
-            for layer_name, layer in ALL_LAYERS.items()
-            if st.sidebar.checkbox(layer_name, True)
+        return pd.read_csv(url, header=0)
+    except Exception as e:
+        st.error("Error al cargar los datos: " + str(e))
+        return None
+
+# Función para convertir varios formatos de fecha al formato dd/mm/aaaa
+def convert_mixed_date_formats(date_str):
+    spanish_to_english_months = {
+        'enero': 'January', 'febrero': 'February', 'marzo': 'March', 'abril': 'April',
+        'mayo': 'May', 'junio': 'June', 'julio': 'July', 'agosto': 'August',
+        'septiembre': 'September', 'octubre': 'October', 'noviembre': 'November', 'diciembre': 'December'
+    }
+    try:
+        if '-' in date_str and len(date_str.split('-')) == 3:
+            day, month, year = date_str.split('-')
+            year = f"20{year}" if len(year) == 2 else year
+            month = spanish_to_english_months.get(month[:3].lower(), month)
+            return dt.strptime(f"{day} {month} {year}", '%d %B %Y').strftime('%d/%m/%Y')
+
+        if ',' in date_str:
+            date_part = ' '.join(date_str.split(',')[1:]).strip()
+            for es, en in spanish_to_english_months.items():
+                date_part = date_part.replace(es, en)
+            return dt.strptime(date_part, '%d de %B de %Y').strftime('%d/%m/%Y')
+
+        return date_str
+    except Exception as e:
+        st.error(f"Error al convertir la fecha: {e}")
+        return date_str
+
+# Aplicación Streamlit
+def main():
+    st.title("Mi Aplicación con Datos de Google Sheets")
+
+    # Carga los datos
+    data = load_data_from_url(sheet_url_csv)
+    data_operaciones = load_data_from_url(sheet_operaciones_url_csv)
+    data_desembolsos = load_data_from_url(sheet_desembolsos_url_csv)
+
+    if data is not None and data_operaciones is not None and data_desembolsos is not None:
+        # Procesamiento de datos
+        date_columns = ['ABSTRACTO', 'CARTA CONSULTA', 'PERFIL', 'PROPUESTA OPERATIVA', 'ACTA NEGOCIACION', 'APROBACIÓN']
+        for col in date_columns:
+            data[col] = data[col].apply(lambda x: convert_mixed_date_formats(x) if isinstance(x, str) else x)
+        data['NO. OPERACION'] = data['NO. OPERACION'].str.replace('-', '', regex=False)
+        data.rename(columns={'NÚMERO': 'NoProyecto'}, inplace=True)
+
+        # Unión de los datos
+        data_merged = pd.merge(data, data_operaciones, on='NoProyecto', how='left')
+        data_merged_total = pd.merge(data_merged, data_desembolsos, on='NoOperacion', how='left')
+
+        # Filtrar el DataFrame para conservar solo las columnas seleccionadas
+        selected_columns = [
+            'NoProyecto', 'NoOperacion', 'Pais', 'Alias', 'SEC', 'ARE', 
+            'CARTA CONSULTA', 'APROBACIÓN', 'PERFIL', 'PROPUESTA OPERATIVA', 'FechaElegibilidad',
+            'FechaVigencia', 'FechaEfectiva', 'Estado_x'
         ]
-        if selected_layers:
-            st.pydeck_chart(
-                pdk.Deck(
-                    map_style=None,
-                    initial_view_state={
-                        "latitude": 37.76,
-                        "longitude": -122.4,
-                        "zoom": 11,
-                        "pitch": 50,
-                    },
-                    layers=selected_layers,
-                )
-            )
-        else:
-            st.error("Please choose at least one layer above.")
-    except URLError as e:
-        st.error(
-            """
-            **This demo requires internet access.**
-            Connection error: %s
-        """
-            % e.reason
-        )
+        filtered_df = data_merged_total[selected_columns]
 
+        # Convertir formatos de fecha en las columnas específicas
+        date_columns_to_convert = ['FechaElegibilidad', 'FechaVigencia', 'FechaEfectiva']
+        for col in date_columns_to_convert:
+            filtered_df[col] = filtered_df[col].apply(lambda x: convert_mixed_date_formats(x) if isinstance(x, str) else x)
 
-st.set_page_config(page_title="Mapping Demo", page_icon="🌍")
-st.markdown("# Mapping Demo")
-st.sidebar.header("Mapping Demo")
-st.write(
-    """This demo shows how to use
-[`st.pydeck_chart`](https://docs.streamlit.io/library/api-reference/charts/st.pydeck_chart)
-to display geospatial data."""
-)
+        # Mostrar el nuevo DataFrame filtrado
+        st.write(filtered_df)
 
-mapping_demo()
+# Ejecutar la aplicación
+if __name__ == "__main__":
+    main()
 
-show_code(mapping_demo)
